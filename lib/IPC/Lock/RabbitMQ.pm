7aa4e0aa5fc7324b7f024a6958e927e3c1650b60
Tomas Doran <bobtfish@bobtfish.net>
2011-02-10 11:30:59 +0000

    Skeleton

---
+++ lib/IPC/Lock/RabbitMQ.pm
@@ -0,0 +1,26 @@
+package IPC::Lock::RabbitMQ;
+use Moose;
+use namespace::autoclean;
+
+1;
+
+=head1 NAME
+
+IPC::Lock::RabbitMQ - 
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=head1 BUGS
+
+=head1 AUTHOR
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 the above author(s).
+
+This sofware is free software, and is licensed under the same terms as perl itself.
+
+=cut
+
